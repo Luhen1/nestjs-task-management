9ae9451882a674e679d276e4322bb8b46594b30f
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2021-10-18 10:26:15 -0300

    Validation - filter and search
    created a error handling exception for filter and search

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTaskFilterDto {
+    // this is my enum data type
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus; // ? sign to show that its optinal to be used
+    // this is my string data type
+    @IsOptional()
+    @IsString()
     search?: string;
 }
