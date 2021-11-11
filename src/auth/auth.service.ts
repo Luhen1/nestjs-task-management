31123cfe2b8e2b07d8a62e45c89996e33b8e47fc
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2021-11-11 17:57:47 -0300

    feature - signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './repository/users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersRepository)
+        private usersRepository: UsersRepository
+    ) {}
+
+    async signUp(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.usersRepository.createUser(AuthCredentialsDto);
+    }
+}
