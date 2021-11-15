ad6590a29333f2e110489f3db42c30db284c1e6d
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2021-11-15 11:10:37 -0300

    added user-task relation and eager = true

--- src/auth/entity/user.entity.ts
+++ src/auth/entity/user.entity.ts
@@ -1,11 +1,18 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Task } from 'src/tasks/entity/task.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
-    @Column({ unique: true})
-    username: string;
-    @Column()
-    password: string;
-} 
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ unique: true })
+  username: string;
+  @Column()
+  password: string;
+
+  // user can have many tasks
+  @OneToMany((_type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
+}
+
+// eager, true -> when you retrieve a user in the database, fetch tasks also that is related with user.
--- src/tasks/entity/task.entity.ts
+++ src/tasks/entity/task.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/entity/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { TaskStatus } from "../task-status.enum";
 
 @Entity()
@@ -14,4 +15,9 @@ export class Task {
 
     @Column()
     status: TaskStatus;
+
+    //many tasks to one user
+    @ManyToOne((_type) => User, (user) => user.tasks, {eager: false})
+    user: User;
 }
+// eager, false -> when you retrieve a task in the database, dont fetch user also that is related with the task.
