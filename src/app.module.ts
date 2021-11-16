772ca5e2b44b016c7e09dbe36f4bb44217206299
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2021-11-16 17:30:37 -0300

    Learnt how to use config management and set them up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TasksModule,
+  imports: [
+    // if you need config module. use this
+    // ConfigModule.forRoot({
+  //   envFilePath: [`.env.stage.${process.env.STAGE}`]
+  // }),
+  TasksModule,
   TypeOrmModule.forRoot({
     type:'postgres',
     host: 'localhost',
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,8 +4,11 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TasksRepository } from './repository/tasks.repository';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
